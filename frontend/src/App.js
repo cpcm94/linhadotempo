87d17b2ee3b70ef18b21e036b55de93ebf47b1e6
Caio Maia <caiopcmaia94@gmail.com>
2021-06-11 16:20:47 -0300

    apollo-client first iteration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,53 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client'
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+const USER_QUERY = gql`
+  query ($id: ID!) {
+    user(id: $id) {
+      id
+      name
+      email
+    }
   }
+`
+const QueryAttempt = () => {
+  const { loading, error, data } = useQuery(USER_QUERY, {
+    variables: { id: '1' },
+  })
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
 
-  handleClick = (api) => (e) => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch('/.netlify/functions/' + api)
-      .then((response) => response.json())
-      .then((json) => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick('hello')}>
-          {loading ? 'Loading...' : 'Call Lambda'}
-        </button>
-        <button onClick={this.handleClick('async-dadjoke')}>
-          {loading ? 'Loading...' : 'Call Async Lambda'}
-        </button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+  return <div>{data}</div>
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>Linha do Tempo</p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
+const client = new ApolloClient({
+  uri: 'https://glacial-mountain-39607.herokuapp.com/graphql',
+  cache: new InMemoryCache(),
+})
+
+const ApolloApp = (Wrapped) => (
+  <ApolloProvider client={client}>
+    <Wrapped />
+  </ApolloProvider>
+)
+const Wrapped = () => {
+  return (
+    <div>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Linha do Tempo</p>
+        <QueryAttempt />
+      </header>
+    </div>
+  )
 }
 
-export default App
+export default () => ApolloApp(Wrapped)
