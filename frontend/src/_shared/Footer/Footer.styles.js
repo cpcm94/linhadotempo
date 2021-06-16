17e6977321051587dcfefb5e45639c7d2d0e8242
Caio Maia <caiopcmaia94@gmail.com>
2021-06-16 18:02:44 -0300

    storybook

---
+++ frontend/src/_shared/Footer/Footer.styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 20%;
+`
+
+export const ButtonWrapper = styled.div`
+  margin: 0.5rem 0.5rem 0.5rem 0px;
+  border: solid 1px #999;
+  color: #655;
+  border-radius: 5px;
+  min-width: 3rem;
+  min-height: 3rem;
+  width: 3rem;
+  height: 3rem;
+  font-size: 3rem;
+  text-align: center;
+  font-family: Karla;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
