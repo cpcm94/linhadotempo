8b87a33d4bea331b3b04ad4298755a9718f6f7e2
Caio Maia <caiopcmaia94@gmail.com>
2021-06-22 20:49:08 -0300

    removed web

--- backend/config/lighthouse.php
+++ backend/config/lighthouse.php
@@ -37,8 +37,7 @@ return [
             // \Illuminate\Cookie\Middleware\EncryptCookies::class,
             // \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             // \Illuminate\Session\Middleware\StartSession::class,
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-'web'
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
 
             // Logs every incoming GraphQL query.
             // \Nuwave\Lighthouse\Support\Http\Middleware\LogGraphQLQueries::class,
