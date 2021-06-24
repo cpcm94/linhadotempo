311c497583a7ec9fd5f71e9634694ee3bed547b6
Caio Maia <caiopcmaia94@gmail.com>
2021-06-24 14:34:04 -0300

    htacess

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*','graphql', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*','graphql', 'sanctum/csrf-cookie', '.well-known/*'],
 
     'allowed_methods' => ['*'],
 
