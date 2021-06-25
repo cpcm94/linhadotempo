9012541c084ef751f267dbda354201616c9a2d42
Caio Maia <caiopcmaia94@gmail.com>
2021-06-25 15:19:30 -0300

    hashing

--- backend/app/GraphQL/Mutations/Login.php
+++ backend/app/GraphQL/Mutations/Login.php
@@ -1,26 +1,34 @@
-<?
+<?php
+
 namespace App\GraphQL\Mutations;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+
 class Login
 {
-    public function __invoke($_, array $args): User
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
     {
-        // if (Auth::attempt($args)) {
-        //     $user = User::find(Auth::id());
-        //     return $user;
-        // }
-        // return null;
-        // // ‘message’ => ‘The username/password combination you entered is invalid.’,
-
-        $guard = Auth::guard(config('sanctum.guard', 'web'));
-
-        if ( ! $guard->attempt($args)) {
-            throw new Error('Invalid credentials.');
+        
+        if (Auth::attempt($args)) {
+            $user = User::find(Auth::id());
+            return $user;
         }
+        return null;
+        // ‘message’ => ‘The username/password combination you entered is invalid.’,
 
-        $user = $guard->user();
+        // $guard = Auth::guard(config('sanctum.guard', 'web'));
 
-        return $user;
+        // if ( ! $guard->attempt($args)) {
+        //     throw new Error('Invalid credentials.');
+        // }
+
+        // $user = $guard->user();
+
+        // return $user;
     }
+
 }
