cacddaaa2808e55585f3f154f8d53fa3585ffc22
Caio Maia <caiopcmaia94@gmail.com>
2021-06-28 15:22:44 -0300

    deleteToken

--- frontend/src/LoginPage/LoginPageLoader.js
+++ frontend/src/LoginPage/LoginPageLoader.js
@@ -3,14 +3,19 @@ import { LoginPage } from './LoginPage'
 import { ME_QUERY } from './ME_QUERY'
 import { useQuery, useMutation } from '@apollo/client'
 import { LOGOUT_MUTATION } from './LOGOUT_MUTATION'
+import { deleteToken } from '../_shared/AuthToken/deleteToken'
 
 export const LoginPageLoader = () => {
   const { data, loading, error } = useQuery(ME_QUERY, {
     notifyOnNetworkStatusChange: true,
   })
 
-  const [logout, { loading: logoutLoading, error: logoutError }] =
-    useMutation(LOGOUT_MUTATION)
+  const [logout, { loading: logoutLoading, error: logoutError }] = useMutation(
+    LOGOUT_MUTATION,
+    {
+      onCompleted: () => deleteToken(),
+    }
+  )
 
   if (loading || logoutLoading) return <span>Loading...</span>
   if (error) console.log(error)
