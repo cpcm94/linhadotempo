b197bf0c5be0983907e75730dc02660f74c58146
Caio Maia <caiopcmaia94@gmail.com>
2021-06-28 15:09:20 -0300

    authentication test

--- frontend/src/LoginPage/LoginPageLoader.js
+++ frontend/src/LoginPage/LoginPageLoader.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import { LoginPage } from './LoginPage'
+import { ME_QUERY } from './ME_QUERY'
+import { useQuery } from '@apollo/client'
 
 export const LoginPageLoader = () => {
+  const { data, loading, error } = useQuery(ME_QUERY, {
+    notifyOnNetworkStatusChange: true,
+  })
+
+  if (loading) return <span>Loading...</span>
+  if (error) console.log(error)
+  if (data && data.me)
+    return (
+      <>
+        <span>{data.me.name}</span>
+        <span>{data.me.id}</span>
+      </>
+    )
+
   return <LoginPage />
 }
