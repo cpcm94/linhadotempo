9a2b1a4f3957349195e59f37de79b48b236bb927
Caio Maia <caiopcmaia94@gmail.com>
2021-06-30 17:29:28 -0300

    update mutation fixed

--- frontend/src/EditTimelinePage/TIMELINE_QUERY.js
+++ frontend/src/EditTimelinePage/TIMELINE_QUERY.js
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client'
 export const TIMELINE_QUERY = gql`
   query Timeline($id: ID!) {
     timeline(id: $id) {
+      id
       name
     }
   }
--- frontend/src/EditTimelinePage/UPDATE_TIMELINE_MUTATION.js
+++ frontend/src/EditTimelinePage/UPDATE_TIMELINE_MUTATION.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
 export const UPDATE_TIMELINE_MUTATION = gql`
-  mutation UpdateTimeline($id: ID, $input: UpdateTimeLineInput!) {
+  mutation UpdateTimeline($id: ID!, $input: UpdateTimelineInput!) {
     updateTimeline(id: $id, input: $input) {
       name
     }
