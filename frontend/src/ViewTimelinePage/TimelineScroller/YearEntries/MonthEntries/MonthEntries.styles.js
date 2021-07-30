02e3a54cb62efaab3c20e5fe516ba37fbe44335e
Caio Maia <caiopcmaia94@gmail.com>
2021-07-30 14:33:12 -0300

    updated design

--- frontend/src/ViewTimelinePage/TimelineScroller/YearEntries/MonthEntries/MonthEntries.styles.js
+++ frontend/src/ViewTimelinePage/TimelineScroller/YearEntries/MonthEntries/MonthEntries.styles.js
@@ -6,11 +6,11 @@ export const MonthWrapper = styled.div`
   flex-direction: row-reverse;
   position: relative;
   z-index: 1;
-  padding: 0.5rem 1rem 0.5rem 2.5rem;
+  padding: 0.5rem 1rem 0.5rem 3rem;
   visibility: ${({ isDisplayEntryMonth }) =>
     isDisplayEntryMonth ? 'hidden' : 'visible'};
   &:before {
-    border-top: 1px solid ${colors.lightBrown};
+    border-top: 1px solid ${colors.lightGrey};
     content: '';
     margin: 0 auto;
     position: absolute;
@@ -45,11 +45,21 @@ export const EntryNameWrapper = styled.div`
   flex: 1;
 `
 export const YearWrapper = styled.div`
-  flex: 0.288;
+  flex: 0.4;
 `
 
 export const MonthSpanWrapper = styled.div`
   flex-direction: row-reverse;
   align-self: flex-end;
-  flex: ${({ hasYear }) => (hasYear ? 0.714 : 0.424)};
+  flex: ${({ hasYear }) => (hasYear ? 0.78 : 0.38)};
+`
+export const DateText = styled.span`
+  background: ${colors.white};
+  padding-right: 0.25rem;
+  color: ${colors.grey};
+`
+
+export const DateInnerWrapper = styled.div`
+  background: ${colors.white};
+  max-width: ${({ hasYear }) => (!hasYear ? 'auto' : '4.5rem')};
 `
