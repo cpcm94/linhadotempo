b2de4a03ac88321177668a4ff00e8744d2e5c55e
Caio Maia <caiopcmaia94@gmail.com>
2021-08-06 17:47:27 -0300

    mutation fix

--- frontend/src/EditUserPage/DELETE_USER_MUTATION.js
+++ frontend/src/EditUserPage/DELETE_USER_MUTATION.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 export const DELETE_USER_MUTATION = gql`
   mutation deleteUser($id: ID!) {
-    deleteUser(id: $id, input: $input) {
+    deleteUser(id: $id) {
       success
       message
     }
