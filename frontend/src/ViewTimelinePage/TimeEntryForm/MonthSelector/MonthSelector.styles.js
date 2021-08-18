3db68f8b0bd667d081ca7fb63bcf08ed1edff534
Caio Maia <caiopcmaia94@gmail.com>
2021-08-18 14:56:42 -0300

    updated entry form

--- frontend/src/ViewTimelinePage/TimeEntryForm/MonthSelector/MonthSelector.styles.js
+++ frontend/src/ViewTimelinePage/TimeEntryForm/MonthSelector/MonthSelector.styles.js
@@ -3,6 +3,8 @@ import { colors } from '../../../_shared/colors'
 export const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
+  background-color: ${colors.lightBrown};
+  border-radius: 5px;
 `
 
 export const MonthWrapper = styled.div`
@@ -10,7 +12,7 @@ export const MonthWrapper = styled.div`
   min-width: 25%;
   border-radius: 5px;
   background-color: ${({ selected }) =>
-    selected ? colors.brown : colors.white};
+    selected ? colors.brown : colors.lightBrown};
   color: ${({ selected }) => (selected ? colors.white : colors.black)};
   cursor: pointer;
 `
