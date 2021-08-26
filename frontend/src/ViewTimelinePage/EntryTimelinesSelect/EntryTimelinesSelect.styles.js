c8ebcbcaa8a04ac12f72b3e322c70e06949673d5
Caio Maia <caiopcmaia94@gmail.com>
2021-08-26 18:41:55 -0300

    margin

--- frontend/src/ViewTimelinePage/EntryTimelinesSelect/EntryTimelinesSelect.styles.js
+++ frontend/src/ViewTimelinePage/EntryTimelinesSelect/EntryTimelinesSelect.styles.js
@@ -12,7 +12,7 @@ export const IconAndNameWrapper = styled.div`
   display: flex;
   align-items: center;
   flex: 1;
-  padding: 1rem 0 1rem 1.25rem;
+  padding: 1rem 0 1rem 0.5rem;
   height: ${({ timelinesIconRow }) => (timelinesIconRow ? '3.5rem' : '3rem')};
   transition: background-color 0.5s ease-out;
 
