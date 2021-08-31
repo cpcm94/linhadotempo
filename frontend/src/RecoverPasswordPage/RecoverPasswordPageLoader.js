9eb32610a7d36cde5a3d2c753513cb51bae3e564
Caio Maia <caiopcmaia94@gmail.com>
2021-08-31 17:46:19 -0300

    expired link

--- frontend/src/RecoverPasswordPage/RecoverPasswordPageLoader.js
+++ frontend/src/RecoverPasswordPage/RecoverPasswordPageLoader.js
@@ -8,19 +8,16 @@ import { useParams } from 'react-router'
 export const RecoverPasswordPageLoader = () => {
   let { hashId } = useParams()
 
-  const { data, loading, error } = useQuery(HASH_USER_QUERY, {
+  const { data, loading } = useQuery(HASH_USER_QUERY, {
     variables: { hash_id: hashId },
     fetchPolicy: 'cache-and-network',
   })
-
-  if (error) {
-    console.error(error)
-    return <ExpiredLinkPage />
-  }
 
   return loading ? (
     <span>Loading...</span>
   ) : data && data.hashUser ? (
     <RecoverPasswordPage user={data.hashUser.user} />
-  ) : null
+  ) : (
+    <ExpiredLinkPage />
+  )
 }
