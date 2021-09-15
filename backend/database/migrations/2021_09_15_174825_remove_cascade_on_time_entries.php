72875c03f05ccb2371fead9dbcd6e68d8dbf55eb
Caio Maia <caiopcmaia94@gmail.com>
2021-09-15 14:49:58 -0300

    cascade removal, schema fix

---
+++ backend/database/migrations/2021_09_15_174825_remove_cascade_on_time_entries.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveCascadeOnTimeEntries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('time_entries', function (Blueprint $table) {
+            $table->dropForeign(['book_id']);
+            $table->dropColumn('book_id');
+            $table->foreignId('book_id')->references('id')->on('books');
+        });
+        }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropForeign(['book_id']);
+        $table->dropColumn('book_id');
+        $table->foreignId('book_id')->references('id')->on('books')->onDelete('cascade');
+
+        }
+}
