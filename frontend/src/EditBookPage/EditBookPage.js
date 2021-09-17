2f6eea211b57e7b8f4f5446acb651f9e65a833c1
Caio Maia <caiopcmaia94@gmail.com>
2021-09-17 18:15:42 -0300

    return button

--- frontend/src/EditBookPage/EditBookPage.js
+++ frontend/src/EditBookPage/EditBookPage.js
@@ -4,12 +4,21 @@ import { Layout } from '../_shared/Layout'
 import { Header } from '../_shared/Header/Header'
 import { Container } from '../_shared/Container'
 import { BookForm } from './BookForm/BookForm'
+import { useHistory } from 'react-router'
 
 export const EditBookPage = ({ bookData }) => {
   const [loading, setLoading] = useState(false)
+  let history = useHistory()
+  const navigateToBooks = () => {
+    history.push('/books')
+  }
   return (
     <Layout>
-      <Header title={'Editar Livro'} showMenuButton={true} loading={loading} />
+      <Header
+        title={'Editar Livro'}
+        returnButton={navigateToBooks}
+        loading={loading}
+      />
       <Container>
         <BookForm bookData={bookData} setLoading={setLoading} />
       </Container>
