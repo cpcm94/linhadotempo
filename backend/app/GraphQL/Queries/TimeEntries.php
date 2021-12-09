81184c4c8d7ee18a1a352262e26144d5a67b0a65
Caio Maia <caiopcmaia94@gmail.com>
2021-12-09 17:38:32 -0300

    time entries query update

--- backend/app/GraphQL/Queries/TimeEntries.php
+++ backend/app/GraphQL/Queries/TimeEntries.php
@@ -11,26 +11,26 @@ class TimeEntries
      */
     public function __invoke($_, array $args)
     {
-        function array_flatten($array = null) {
-            $result = array();
+        // function array_flatten($array = null) {
+        //     $result = array();
         
-            if (!is_array($array)) {
-                $array = func_get_args();
-            }
+        //     if (!is_array($array)) {
+        //         $array = func_get_args();
+        //     }
         
-            foreach ($array as $key => $value) {
-                if (is_array($value)) {
-                    $result = array_merge($result, array_flatten($value));
-                } else {
-                    $result = array_merge($result, array($key => $value));
-                }
-            }
+        //     foreach ($array as $key => $value) {
+        //         if (is_array($value)) {
+        //             $result = array_merge($result, array_flatten($value));
+        //         } else {
+        //             $result = array_merge($result, array($key => $value));
+        //         }
+        //     }
         
-            return $result;
-        }
+        //     return $result;
+        // }
         
-        $res = array_flatten($args);
-        $entries = TimeEntry::leftJoin('time_entry_timeline', 'time_entries.id','time_entry_timeline.time_entry_id')->whereIn('timeline_id', $res)->get()->unique('time_entry_id');
+        // $res = array_flatten($args);
+        $entries = TimeEntry::leftJoin('time_entry_timeline', 'time_entries.id','time_entry_timeline.time_entry_id')->whereIn('timeline_id', $args)->get()->unique('time_entry_id');
         foreach ($entries as $entry) {
             $entry['id'] = $entry['time_entry_id'];
         }
