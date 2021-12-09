131e93389a45e7bc2ee6732de9f552baeb2565cc
Caio Maia <caiopcmaia94@gmail.com>
2021-12-09 17:44:06 -0300

    small fix time_entries query

--- backend/app/GraphQL/Queries/TimeEntries.php
+++ backend/app/GraphQL/Queries/TimeEntries.php
@@ -30,7 +30,7 @@ class TimeEntries
         // }
         
         // $res = array_flatten($args);
-        $entries = TimeEntry::leftJoin('time_entry_timeline', 'time_entries.id','time_entry_timeline.time_entry_id')->whereIn('timeline_id', $args)->get()->unique('time_entry_id');
+        $entries = TimeEntry::leftJoin('time_entry_timeline', 'time_entries.id','time_entry_timeline.time_entry_id')->whereIn('timeline_id', $args['timeline_ids'])->get()->unique('time_entry_id');
         foreach ($entries as $entry) {
             $entry['id'] = $entry['time_entry_id'];
         }
